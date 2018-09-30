6294879f1132e07cd2d23834915dcf582c37e404
Cristian Bazaes <cbazaes.isw@gmail.com>
2018-09-30 16:16:27 -0300

    Se agrega verde al código

--- main.py
+++ main.py
@@ -5,10 +5,22 @@ import numpy as np
 from datetime import datetime
 import collections
 
+# Formato de texto para mostrar en cada cuadro la
+# desviación estándar de cambio entre cuadros.
 text_format_stdev = "Desviacion estandar {}"
+
+# Lista de largo fijo para calcular la desviación estándar
+# en función del porcentaje de cambio de los últimos cuadros
+# procesados.
 percentages = collections.deque(maxlen=10)
+
+# Máxima desviación estándar tolerable, luego de la cual
+# se considera que los cuadros tienen movimiento.
 TOLERANCIA_MOVIMIENTO = 0.13
 
+# Se establece comunicación con la cámara
+# se obtiene el alto y ancho de cada cuadro
+# y se captura el primer cuadro.
 video_capture = cv2.VideoCapture(0)
 frameWidth = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 frameHeight = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -20,13 +32,20 @@ while True:
         sleep(5)
         pass
 
-    # Capture frame-by-frame
+    # Captura un cuadro
     ret, frame = video_capture.read()
 
+    # Se determina el porcentaje de diferencia entre el cuadro actual
+    # y el capturado en la iteración anterior. Además se determina
+    # la desviación estándar en función de la cual se determinará
+    # si hay movimiento entre cuadro y cuadro.
     perc = util.get_difference_percentage(frame, prev_frame)
     percentages.append(perc)
     stdev = np.std(percentages)
 
+    # Si la desviación estándar de cambio entre cuadros es mayor
+    # o igual al factor de tolerancia se dibuja un círculo rojo
+    # en el cuadro actual.
     if (stdev >= TOLERANCIA_MOVIMIENTO):
         cv2.circle(
             frame,
@@ -36,6 +55,8 @@ while True:
             color=(0, 0, 255)
         )
 
+    # Escribo la desviación estándar de cambio entre cuadros y
+    # la fecha/hora de captura
     img_text_stdev = text_format_stdev.format(stdev)
     cv2.putText(
         img=frame,
@@ -54,14 +75,16 @@ while True:
         color=(0, 255, 0)
     )
 
-    # Display the resulting frame
+    # Se muestra el frame resultante
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+    # Capturo el frame actual para compararlo con el
+    # frame capturado en la siguiente iteración
     prev_frame = frame
 
-# When everything is done, release the capture
+# Cuando todo ha finalizado, libero la cámara
 video_capture.release()
 cv2.destroyAllWindows()
--- util.py
+++ util.py
@@ -3,10 +3,12 @@ import io
 import cv2
 import numpy as np
 
+
 def convertCvFrame2Bytes(cvFrame):
     stream = convertCvFrame2Stream(cvFrame)
     bin_img = stream.getvalue()
     return bin_img
+
 
 def convertCvFrame2Stream(cvFrame):
     pil_img = Image.fromarray(cvFrame)
@@ -14,12 +16,10 @@ def convertCvFrame2Stream(cvFrame):
     pil_img.save(stream, format='JPEG')
     return stream
 
+
 def get_difference_percentage(cvFrame1, cvFrame2):
-    # cvFrame1 = cv2.imread("D:\Documentos\drone\DJI MAVIC PRO\cajon_del_maipo\DJI_0002.JPG")
-    # cvFrame2 = cv2.imread("D:\Documentos\drone\DJI MAVIC PRO\cajon_del_maipo\DJI_0002.JPG")
     res = cv2.absdiff(cvFrame1, cvFrame2)
     res = res.astype(np.uint8)
-    percentage = (np.count_nonzero(res) * 100)/ res.size
+    percentage = (np.count_nonzero(res) * 100) / res.size
 
     return percentage
-
