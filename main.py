1defd8f0bfba641d43069cc2a669269d56f19f3f
Cristian Bazaes <cbazaes.isw@gmail.com>
2018-09-30 16:07:09 -0300

    - se reemplaza el porcentaje de cambio entre cuadro y cuadro por la desviación estándar de los 10 últimos cuadros procesados - se agrega la fecha y la hora de captura de cada cuadro - se agrega un círculo rojo al cuadro cuando se sobre pase el nivel de tolerancia de movimiento

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import cv2
 from time import sleep
 import util
+import numpy as np
+from datetime import datetime
+import collections
+
+text_format_stdev = "Desviacion estandar {}"
+percentages = collections.deque(maxlen=10)
+TOLERANCIA_MOVIMIENTO = 0.13
 
 video_capture = cv2.VideoCapture(0)
+frameWidth = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+frameHeight = int(video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 ret, prev_frame = video_capture.read()
 
 while True:
@@ -15,14 +24,42 @@ while True:
     ret, frame = video_capture.read()
 
     perc = util.get_difference_percentage(frame, prev_frame)
-    print("Porcentaje de movimiento en la captura {}%".format(perc),end='\r')
+    percentages.append(perc)
+    stdev = np.std(percentages)
+
+    if (stdev >= TOLERANCIA_MOVIMIENTO):
+        cv2.circle(
+            frame,
+            center=(int(frameWidth / 15), int(frameHeight / 15)),
+            radius=10,
+            thickness=-1,
+            color=(0, 0, 255)
+        )
+
+    img_text_stdev = text_format_stdev.format(stdev)
+    cv2.putText(
+        img=frame,
+        text=img_text_stdev,
+        org=(10, int(frameHeight - 10)),
+        fontFace=cv2.FONT_HERSHEY_PLAIN,
+        fontScale=1,
+        color=(0, 255, 0)
+    )
+    cv2.putText(
+        img=frame,
+        text=str(datetime.now()),
+        org=(int(frameWidth / 5) * 3, int(frameHeight - 10)),
+        fontFace=cv2.FONT_HERSHEY_PLAIN,
+        fontScale=1,
+        color=(0, 255, 0)
+    )
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
+
     prev_frame = frame
 
 # When everything is done, release the capture
